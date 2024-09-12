35881482e2658fe957283bdc8b9edd9e91b3c7d8
dilshan-dulanjana <dilshanguruge99930@gmail.com>
2024-09-13 00:28:20 +0530

    ok

--- app.js
+++ app.js
@@ -18,4 +18,39 @@ function updateLocalTime() {
   updateLocalTime();
   
   setInterval(updateLocalTime, 1000);
+
+
+  document.getElementById("btnsearch").onclick = function(){
+
+    let location = document.getElementById("txtlocation").value;
+
+     
+    let reop ={
+      method:"Get"
+
+    }
+    fetch(`http://api.weatherapi.com/v1//current.json?key=9da2e733fd05417db69161750241209&q=${location}`,reop)
+       .then(response=>response.json())
+       .then(data =>{
+        console.log(data);
+        document.getElementById("location").innerHTML=data["location"]["name"];
+        document.getElementById("temp_c").innerHTML=data["current"]["temp_c"];
+        document.getElementById("dscription").innerHTML=data["current"]["condition"]["text"];
+        document.getElementById("country").innerHTML=data["location"][ "country"];
+        document.getElementById("tz_id").innerHTML=data["location"]["tz_id"];
+        document.getElementById("humidity").innerHTML=data["current"]["humidity"];
+        document.getElementById("wind_kph").innerHTML=data["current"]["wind_kph"];
+        document.getElementById("url").innerHTML=data["current"]["condition"]["text"];
+        document.getElementById("wind_kph").innerHTML=data["current"]["wind_kph"];
+        document.getElementById("region").innerHTML=data["location"]["region"];
+        document.getElementById("countrys").innerHTML=data["location"][ "country"];
+        document.getElementById("lon").innerHTML=data["location"]["lon"];
+        document.getElementById("lat").innerHTML=data["location"]["lat"];
+
+        document.getElementById("weatherIcon").src=data["current"]["condition"]["icon"];
+        document.getElementById("local-times").innerHTML=data["location"]["localtime"];
+    
+       })
+       .then(error=>console.log(" Error",error))
+  };
   
